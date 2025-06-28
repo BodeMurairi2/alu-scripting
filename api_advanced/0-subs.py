c4d948598a133d2723b43d6bcb5f891690a371f6
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-28 16:41:59 +0200

    Task 0

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''
+This script returns the number of subscribers
+for a specific subredits
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    headers = {
+        'User-Agent': 'linux:subcountscript:v1.0 (by /u/bodemurairi)'
+    }
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url=api_url, headers=headers, timeout=10)
+
+    if response.status_code != 200:
+        print(f"Errors when loading data. Error codes: {response.status_code}")
+        return 0
+
+    response_data = response.json()
+    return response_data['data']['subscribers']
