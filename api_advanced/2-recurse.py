582be96907200c028abbbb1afe27c127f12c7224
BodeMurairi2 <b.murairi@alustudent.com>
2025-07-15 00:05:57 +0200

    task 02

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""
-Writing a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit, the function returns None.
-"""
+"""Module that consumes the Reddit API and returns a list containing the
+titles of all hot articles for a given subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ function that queries the Reddit API and returns a list containing
-        the titles of all hot articles for a given subreddit. If no results
-        are found for the given subreddit, the function should return None.
+def recurse(subreddit, hot_list=[], n=0, after=None):
+    """ queries the Reddit API and returns a list containing the titles of
+    all hot articles for a given subreddit
+
+    The Reddit API uses pagination for separating pages of responses.
+    If not a valid subreddit, return None.
+
+    Args:
+        subreddit (str): subreddit.
+        hot_list (list, optional): list of titles. Defaults to [].
+
+    Returns:
+        list: list of titles.
     """
-    headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0)\
-                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100\
-                Safari/537.36'}
-    params = {'limit': 100, 'after': after}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    res = requests.get(url, headers=headers, params=params)
-    if res.status_code == 404:
-        return None
-    children = res.json().get('data').get('children')
-    for child in children:
-        hot_list.append(child.get('data').get('title'))
-    after = res.json().get('data').get('after')
-    if after is None:
+    headers = {'user-agent': 'custom'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        r = r.json()
+        for post in r.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if r.get('data').get('after'):
+            recurse(subreddit, hot_list)
         return hot_list
-    return recurse(subreddit, hot_list, after)
+    else:
+        return None
