16f094c6650da7e74663dc977eae32189b7d5596
BodeMurairi2 <b.murairi@alustudent.com>
2025-07-14 23:07:09 +0200

    Task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""Module that returns number of subscribers 
-from a given subreddit
+"""
+A function that returns the numbers of subscribers
 """
 
 import requests
@@ -9,12 +9,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns number of subscribers for a subreddit"""
+
     headers = {
         'User-Agent': 'linux:subcountscript:v1.0 (by /u/bodemurairi'
     }
+
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = requests.get(url=api_url, headers=headers, timeout=10, allow_redirects=False)
+    response = requests.get(url=api_url,
+                            headers=headers,
+                            timeout=10,
+                            allow_redirects=False)
 
     response.raise_for_status()
 
